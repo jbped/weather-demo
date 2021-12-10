6dc89027ae72723c2ee0b22d7e775d6358adc656
Jake P <76881086+jbped@users.noreply.github.com>
2021-12-10 16:56:11 -0700

    adding 3 day forecast to the weather demo

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,11 @@
 $(function () {
   // React to hitting enter in the text box instead of clicking submit.
-  $("#search-weather").submit(function(e) {
+  $('#search-weather').submit(function (e) {
     e.preventDefault();
     clearResult('');
 
     // Get the weather info for the selected search location.
-    $.get("/weather.php?query=" + $("#search").val(), function (data) {
+    $.get('/weather.php?query=' + $('#search').val(), function (data) {
       // Show weather results.
       if (data.temp_f) {
         showResult(data);
@@ -16,20 +16,38 @@ $(function () {
   });
 
   function clearResult($msg = 'Sorry Charlie! Not a valid input.') {
-    $(".weather_icon").attr("src", "images/trans.png");
-    $("#location").hide();
-    $("#city").text();
-    $("#state").text();
-    $(".result .description").html($msg);
+    $('.weather_icon').attr('src', 'images/trans.png');
+    $('.details').hide();
+    $('#location').hide();
+    $('#city').text();
+    $('#state').text();
+    $('.result .description').html($msg);
   }
 
   function showResult(data) {
-    $(".weather_icon").attr("src", data.icon_url);
-    $("#location").show();
-    $("#city").text(data.city);
-    $("#state").text(data.state);
-    var desc = data.weather + " and " + data.temp_f + "&deg; F";
-    $(".result .description").html(desc);
-  }
+    $('.weather_icon').attr('src', data.icon_url);
+    $('.details').show();
+    $('#location').show();
+    $('#city').text(data.city);
+    $('#state').text(data.state);
 
+    for (let i = 0; i < data.forecast.length; i++) {
+      const dayContainer = $('<div></div>').attr('id', `forecast-${i}`);
+      const imageData = $('<img/>').attr(
+        'src',
+        data.forecast[i].day.condition.icon
+      );
+      const dateData = $('<p></p>').text(data.forecast[i].date);
+      const descriptionData = $('<p></p>').text(
+        data.forecast[i].day.condition.text
+      );
+      $(dayContainer).append(dateData);
+      $(dayContainer).append(imageData);
+      $(dayContainer).append(descriptionData);
+      $('.forecast').append(dayContainer);
+    }
+
+    var desc = data.weather + ' and ' + data.temp_f + '&deg; F';
+    $('.result .description').html(desc);
+  }
 });
--- weather.php
+++ weather.php
@@ -1,30 +1,33 @@
 <?php
-  /**
-   * This page talks to weatherapi.com.
-   */
 
-  // API URL.
-  // You can register for free to get your own api key (fb0... below). 
-  $url = "http://api.weatherapi.com/v1/current.json?key=fb0b4c5c22d04c22be2202032210112&aqi=no&q=";
+/**
+ * This page talks to weatherapi.com.
+ */
 
-  // Check and filter query parameters.
-  $query = filter_input(INPUT_GET, 'query', FILTER_DEFAULT);
+// API URL.
+// You can register for free to get your own api key (fb0... below). 
+$currentUrl = "http://api.weatherapi.com/v1/current.json?key=fb0b4c5c22d04c22be2202032210112&aqi=no&q=";
+$forecastUrl = "http://api.weatherapi.com/v1/forecast.json?key=fb0b4c5c22d04c22be2202032210112&aqi=no&days=3&q=";
 
-  if ($query) {
-    $query = urlencode($query);
+// Check and filter query parameters.
+$query = filter_input(INPUT_GET, 'query', FILTER_DEFAULT);
 
-    // Call the api with the user input query (zip, city, etc.).
-    $data = file_get_contents($url . $query);
-    $parsed = json_decode($data);
+if ($query) {
+  $query = urlencode($query);
 
-    // You can use PHP's error_log to output a string to the log for debugging.
-    // error_log($parsed->location->name);
+  // Call the api with the user input query (zip, city, etc.).
+  $data = file_get_contents($forecastUrl . $query);
+  $parsed = json_decode($data);
 
-    $result['weather'] = $parsed->current->condition->text;
-    $result['temp_f'] = $parsed->current->temp_f;
-    $result['icon_url'] = $parsed->current->condition->icon;
-    $result['city'] = $parsed->location->name;
-    $result['state'] = $parsed->location->region;
-    header('Content-Type: application/json');
-    echo json_encode($result);
-  }
+  // You can use PHP's error_log to output a string to the log for debugging.
+  // error_log($parsed->location->name);
+
+  $result['weather'] = $parsed->current->condition->text;
+  $result['temp_f'] = $parsed->current->temp_f;
+  $result['icon_url'] = $parsed->current->condition->icon;
+  $result['city'] = $parsed->location->name;
+  $result['state'] = $parsed->location->region;
+  $result['forecast'] = $parsed->forecast->forecastday;
+  header('Content-Type: application/json');
+  echo json_encode($result);
+}
